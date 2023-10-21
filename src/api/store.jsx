9a199913f5b5a02dd74736542352e9f73a0a6720
Sonthep <gan14tap@gmail.com>
2023-10-21 12:17:12 +0700

    final

--- src/api/store.jsx
+++ src/api/store.jsx
@@ -24,13 +24,14 @@ function Store() {
   }
 
   return (
-    <div>
-      <h1>Product List</h1>
-      <ul className='grid grid-flow-rows grid-cols-3 gap-4'>
+    <div className='flex item-center justify-center'>
+
+      <ul className='grid grid-flow-rows grid-cols-3 gap-[5rem] mt-10'>
         {products.map(product => (
-          <li key={product.id} className='text-red-500 '>
-            {product.title}
+          <li key={product.id} className=' rounded-lg text-center border w-[15rem]'>
+            <p className='mb-4 mt-2'>{product.title}</p>
             <img src={product.image} alt="" className='' />
+            <p className='mt-4'>Prize {product.price} $</p>
           </li>
         ))}
       </ul>
